c2154e49e45b3dfed932619207fcfac94c366187
lparolari <luca.parolari23@gmail.com>
2021-08-06 21:12:43 +0200

    feat(dataloader): dataset and dataloader baseline

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
 import argparse
+import functools
 import logging
 
-from tests.mock_tensor import MockTensor
+import torch
+import torchtext
+
 from weakvtg.config import parse_configs
+from weakvtg.dataset import VtgDataset, collate_fn
 from weakvtg.math import get_argmax, get_max
+from weakvtg.tokenizer import get_torchtext_tokenizer_adapter, get_nlp
 from weakvtg.train import train
+from weakvtg.vocabulary import load_vocab
+
+
+class MockTensor:
+    def __init__(self, value):
+        self.value = value
+
+    def __format__(self, format_spec):
+        return f"{self.value:{format_spec}}"
+
+    def __str__(self):
+        return str(self.value)
+
+    def item(self):
+        return self.value
+
+    def backward(self):
+        pass
 
 
 def parse_args():
@@ -26,11 +49,18 @@ if __name__ == "__main__":
 
     logging.info(f"Model started with following parameters: {configs}")
 
+    dataset = VtgDataset("data/referit_raw/preprocessed", "data/referit_raw/train.txt")
+
+    tokenizer = torchtext.data.utils.get_tokenizer(tokenizer=get_torchtext_tokenizer_adapter(get_nlp()))
+    vocab = load_vocab("data/referit_raw/vocab.json")
+
+    collate_function = functools.partial(collate_fn, tokenizer=tokenizer, vocab=vocab)
+
     # TODO: replace mock
     def c(_1, _2):
         return MockTensor(1.), MockTensor(2.), MockTensor(3.), MockTensor(4.)
     from unittest import mock
-    train_loader = [{"id": range(10)}, {"id": range(10)}, {"id": range(10)}, {"id": range(10)}]
+    train_loader = torch.utils.data.DataLoader(dataset, collate_fn=collate_function)
     valid_loader = [{"id": range(10)}, {"id": range(10)}, {"id": range(10)}, {"id": range(10)}]
     model = mock.Mock()
     optimizer = mock.Mock()
@@ -43,4 +73,3 @@ if __name__ == "__main__":
 
     logging.info(f"Best hist validation loss at epoch {get_argmax(valid_loss)}: {get_max(valid_loss)}")
     logging.info(f"Best hist validation accuracy at epoch {get_argmax(valid_accuracy)}: {get_max(valid_accuracy)}")
-
---
+++ weakvtg/dataset.py
@@ -0,0 +1,105 @@
+import os
+import random
+
+import torch
+from torch.utils.data import Dataset
+from torch.utils.data.dataset import T_co
+
+from weakvtg import iox, bbox
+from weakvtg.padder import get_phrases_tensor, get_padded_examples, get_number_examples, get_max_length_examples, \
+    get_max_length_phrases
+from weakvtg.utils import pivot
+
+
+class VtgDataset(Dataset):
+    def __init__(self, data_filepath: str, idx_filepath: str):
+        super().__init__()
+
+        self.data_filepath = data_filepath
+        self.idx_filepath = idx_filepath
+        
+        self.idx = self._load_index()
+        self.examples = self._load_examples()
+        self.n = len(self.examples)
+
+    def __getitem__(self, index) -> T_co:
+        caption_filename = "{}".format(self.examples[index])
+        image_filename = "{}_img.pickle".format(self.examples[index].split("_")[0])
+        
+        caption = iox.load_pickle(os.path.join(self.data_filepath, caption_filename))
+        image = iox.load_pickle(os.path.join(self.data_filepath, image_filename))
+        
+        example = {**caption, **image}
+        example = process_example(example)
+
+        return example
+
+    def __len__(self):
+        return self.n
+
+    def _load_index(self):
+        return read_index(self.idx_filepath)
+    
+    def _load_examples(self):
+        examples = sorted(os.listdir(self.data_filepath))
+        examples = filter(lambda x: "img" not in x, examples)
+        examples = filter(lambda x: x.split("_")[0] in self.idx, examples)
+        return list(examples)
+
+
+def collate_fn(batch, tokenizer, vocab):
+    batch = pivot(batch)
+
+    phrases = batch['phrases']  # [b, n_ph, n_words]
+    phrases_2_crd = batch['phrases_2_crd']  # [b, n_ph, 4]
+
+    phrases, phrases_mask = get_phrases_tensor(phrases, tokenizer=tokenizer, vocab=vocab)
+    phrases_2_crd, phrases_2_crd_mask = get_padded_examples(phrases_2_crd,
+                                                            padding_value=0,
+                                                            padding_dim=(
+                                                                get_number_examples(phrases_2_crd),
+                                                                get_max_length_examples(phrases_2_crd),
+                                                                get_max_length_phrases(phrases_2_crd)))
+
+    return {
+        'id': torch.tensor(batch['id'], dtype=torch.long),
+        'image_w': torch.tensor(batch['image_w'], dtype=torch.long),
+        'image_h': torch.tensor(batch['image_h'], dtype=torch.long),
+        'pred_n_boxes': torch.tensor(batch['pred_n_boxes'], dtype=torch.int),
+        'pred_boxes': torch.tensor(batch['pred_boxes'], dtype=torch.float32),
+        'pred_cls_prob': torch.tensor(batch['pred_cls_prob'], dtype=torch.float32),
+        'pred_attr_prob': torch.tensor(batch['pred_attr_prob'], dtype=torch.float32),
+        'pred_boxes_features': torch.tensor(batch['pred_boxes_features'], dtype=torch.float32),
+        'pred_active_box_index': torch.tensor(batch['pred_active_box_index'], dtype=torch.long),
+        'phrases': phrases,
+        'phrases_mask': phrases_mask,
+        'phrases_2_crd': phrases_2_crd,
+        'phrases_2_crd_mask': phrases_2_crd_mask,
+    }
+
+
+def read_index(filename: str):
+    with open(filename, "r") as f:
+        return [line.strip("\n") for line in f.readlines()]
+
+
+def process_example(example, n_boxes_to_keep: int = 100, n_active_box: int = 3):
+    example["id"] = int(example["id"])
+    example["phrases_2_crd"] = bbox.scale_bbox(example["phrases_2_crd"], example["image_w"], example["image_h"])
+    example["pred_boxes"] = bbox.scale_bbox(example["pred_boxes"], example["image_w"], example["image_h"])
+
+    pred_n_boxes = example['pred_n_boxes']
+    n_boxes_class = len(example['pred_attr_prob'][0])
+    n_boxes_attr = len(example['pred_cls_prob'][0])
+    n_boxes_features = len(example['pred_boxes_features'][0])
+    n_boxes_to_gen = n_boxes_to_keep - pred_n_boxes
+    example['pred_n_boxes'] = n_boxes_to_keep
+    example['pred_boxes'] = example['pred_boxes'] + [[0] * 4 for i in range(n_boxes_to_gen)]
+    example['pred_boxes_features'] = example['pred_boxes_features'] + [[0] * n_boxes_features for i in
+                                                                       range(n_boxes_to_gen)]
+    example['pred_attr_prob'] = example['pred_attr_prob'] + [[0] * n_boxes_class for i in range(n_boxes_to_gen)]
+    example['pred_cls_prob'] = example['pred_cls_prob'] + [[0] * n_boxes_attr for i in range(n_boxes_to_gen)]
+
+    example['pred_active_box_index'] = [random.randrange(0, pred_n_boxes) for _ in range(n_active_box)]
+
+    return example
