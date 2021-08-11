ae86d279bcc2ee9113d00b76ddeebe4c92294cfa
lparolari <luca.parolari23@gmail.com>
2021-08-11 14:01:06 +0200

    feat(mode): get phrases features

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,7 +1,10 @@
+from unittest import mock
+
 import torch
 import torch.nn.functional as F
 
-from weakvtg.model import get_image_features
+from weakvtg.mask import get_synthetic_mask
+from weakvtg.model import get_image_features, get_phrases_features
 
 
 def test_get_image_features():
@@ -13,3 +16,27 @@ def test_get_image_features():
     actual = get_image_features(boxes, boxes_feat)
 
     assert torch.equal(expected, actual)
+
+
+def test_get_phrases_features():
+    phrases = torch.Tensor([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    phrases_mask = torch.Tensor([[1, 1, 1], [1, 0, 0], [0, 0, 0]])
+    phrases_length = torch.sum(phrases_mask.int(), dim=-1)
+    mask = get_synthetic_mask(phrases_mask)
+
+    embedding_network = mock.Mock()
+    embedding_network.return_value = phrases
+
+    recurrent_network = mock.Mock()
+    recurrent_network.return_value = phrases
+
+    actual = get_phrases_features(phrases, phrases_mask, embedding_network, recurrent_network)
+    expected = torch.masked_fill(phrases, mask == 0, value=0)
+
+    assert torch.equal(actual, expected)
+    embedding_network.assert_called_with(phrases)
+    recurrent_network.assert_called_once()
+
+    # recurrent_network.assert_called_with(phrases, phrases_length, mask) throws an error
+
+
--- weakvtg/model.py
+++ weakvtg/model.py
@@ -4,6 +4,7 @@ import torch.nn.functional as F
 
 from weakvtg import anchors
 from weakvtg.mask import get_synthetic_mask
+from weakvtg.utils import identity
 
 
 class Model(nn.Module):
@@ -28,6 +29,12 @@ class MockModel(Model):
 
 
 class WeakVtgModel(Model):
+    def __init__(self, phrases_embedding_net=identity, phrases_recurrent_net=identity):
+        super().__init__()
+
+        self.phrases_embedding_net = phrases_embedding_net
+        self.phrases_recurrent_net = phrases_recurrent_net
+
     def forward(self, batch):
         boxes = batch["pred_boxes"]  # [b, n_boxes, 4]
         boxes_features = batch["pred_boxes_features"]  # [b, n_boxes, 2048]
@@ -35,6 +42,7 @@ class WeakVtgModel(Model):
         phrases_mask = batch["phrases_mask"]  # [b, n_ph, n_words]
 
         img_x = get_image_features(boxes, boxes_features)
+        phrases_x = get_phrases_features(phrases, phrases_mask, self.phrases_embedding_net, self.phrases_recurrent_net)
         # TODO
 
 
@@ -52,3 +60,25 @@ def get_image_features(boxes, boxes_feat):
     area = (boxes_tlhw[..., 2] * boxes_tlhw[..., 3]).unsqueeze(-1)  # [*1, 1]
 
     return torch.cat([boxes_feat, boxes, area], dim=-1)
+
+
+def get_phrases_features(phrases, phrases_mask, embedding_network, recurrent_network):
+    """
+    Embed phrases and apply LSTM network on embeddings.
+
+    :param phrases: A [*, d1, d2] tensor
+    :param phrases_mask: A [*, d1, d2] tensor
+    :param embedding_network: A function of phrases tensor
+    :param recurrent_network: A function of embedding tensor, phrases length and synthetic mask
+    :return: A [*, d1, emb_p] tensor
+    """
+    mask = get_synthetic_mask(phrases_mask)
+
+    phrases_length = torch.sum(phrases_mask.int(), dim=-1)  # [*, d1]
+
+    phrases_embedding = embedding_network(phrases)  # [*, d1, d2, fp]
+
+    phrases_x = recurrent_network(phrases_embedding, phrases_length, mask)  # [*, d1, emb_p]
+    phrases_x = torch.masked_fill(phrases_x, mask == 0, value=0)
+
+    return phrases_x
