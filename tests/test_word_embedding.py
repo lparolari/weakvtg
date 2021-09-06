4db436960c1106a5b4b6ac8145fa940068c20da8
lparolari <luca.parolari23@gmail.com>
2021-09-06 14:40:43 +0200

    test: glove embedding similarity

--- tests/test_word_embedding.py
+++ tests/test_word_embedding.py
@@ -1,4 +1,5 @@
 import pytest
+import torch.nn.functional
 import torchtext
 
 from weakvtg import classes
@@ -13,6 +14,22 @@ def test_word_embedding_given_classes(vocab, glove_words):
     out_of_vocabulary = list(filter(lambda word: word not in glove_words, vocab))
 
     assert len(out_of_vocabulary) == 295
+
+
+def test_word_embedding_similarity(glove_embeddings):
+    def _get_similarity(word1, word2):
+        w1_index = glove_embeddings.stoi[word1]
+        w2_index = glove_embeddings.stoi[word2]
+
+        w1 = glove_embeddings.vectors[w1_index]
+        w2 = glove_embeddings.vectors[w2_index]
+
+        return torch.nn.functional.cosine_similarity(w1, w2, dim=-1)
+
+    assert round(_get_similarity("person", "woman").item(), 4) == pytest.approx(0.5618)
+    assert round(_get_similarity("person", "man").item(), 4) == pytest.approx(0.5557)
+    assert round(_get_similarity("man", "woman").item(), 4) == pytest.approx(0.7402)
+    assert round(_get_similarity("person", "eggs").item(), 4) == pytest.approx(0.215)
 
 
 @pytest.fixture
