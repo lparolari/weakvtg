b9f51571f04126da4bb690292925525cdf68e4f4
lparolari <luca.parolari23@gmail.com>
2021-09-15 15:06:41 +0200

    test: fix word embedding cache path

--- tests/vocabulary/test_get_word_embedding.py
+++ tests/vocabulary/test_get_word_embedding.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 import torchtext.vocab
 
@@ -6,12 +8,28 @@ from weakvtg.vocabulary import get_word_embedding
 Vectors = torchtext.vocab.Vectors
 
 
-def test_get_word_embedding():
-    assert isinstance(get_word_embedding('glove'), Vectors)
-    assert isinstance(get_word_embedding('w2v'), Vectors)
+def test_vector_cache_exists(vector_cache):
+    assert os.path.isdir(vector_cache)
 
-    with pytest.raises(ValueError):
-        get_word_embedding('foo')
 
+def test_get_word_embedding_given_glove(vector_cache):
+    assert isinstance(get_word_embedding('glove', cache=vector_cache), Vectors)
+
+
+def test_get_word_embedding_given_w2v(vector_cache):
+    assert isinstance(get_word_embedding('w2v', cache=vector_cache), Vectors)
+
+
+def test_get_word_embedding_given_unknown(vector_cache):
     with pytest.raises(ValueError):
-        get_word_embedding('w2v', 400)
+        get_word_embedding('foo', cache=vector_cache)
+
+
+def test_get_word_embedding_given_w2v_wrong_dim(vector_cache):
+    with pytest.raises(ValueError):
+        get_word_embedding('w2v', 400, cache=vector_cache)
+
+
+@pytest.fixture
+def vector_cache():
+    return os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "..", ".vector_cache")
