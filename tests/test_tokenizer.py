df79fb97342d40fbdb36ea8b6bf3aec489123521
lparolari <luca.parolari23@gmail.com>
2021-09-17 13:53:44 +0200

    feat(tokenizer): extract noun phrases

--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -1,7 +1,8 @@
 import pytest
 import spacy
 
-from weakvtg.tokenizer import get_nlp, get_torchtext_tokenizer_adapter
+from weakvtg.tokenizer import get_nlp, get_torchtext_tokenizer_adapter, get_noun_phrases, get_text, noun_chunk_iter, \
+    root_chunk_iter
 
 
 def text(doc):
@@ -36,3 +37,42 @@ def test_spacy_special_cases(nlp):
 def test_torchtext_adapter(nlp):
     adapted_nlp = get_torchtext_tokenizer_adapter(nlp)
     assert adapted_nlp("hills/cliffs") == text(nlp("hills/cliffs"))
+
+
+def test_noun_chunk_iter(nlp):
+    doc = nlp("A phrase with another phrase occurs..")
+    it = noun_chunk_iter(doc)
+
+    assert next(it).text == "A phrase"
+    assert next(it).text == "another phrase"
+    with pytest.raises(StopIteration):
+        next(it)
+
+
+def test_get_noun_phrases_given_noun_chunking(nlp):
+    doc = nlp("The cat and the dog sleep in the basket near the door with a person.")
+    np = get_noun_phrases(doc, f_chunking=noun_chunk_iter)
+
+    assert len(np) == 5
+    assert np[0] == "The cat"
+    assert np[1] == "the dog sleep"
+    assert np[2] == "the basket"
+    assert np[3] == "the door"
+    assert np[4] == "a person"
+
+
+def test_get_noun_phrases_given_root_chunking(nlp):
+    doc = nlp("The cat and the dog sleep in the basket near the door with a person.")
+    np = get_noun_phrases(doc, f_chunking=root_chunk_iter)
+
+    assert len(np) == 5
+    assert np[0] == "cat"
+    assert np[1] == "sleep"
+    assert np[2] == "basket"
+    assert np[3] == "door"
+    assert np[4] == "person"
+
+
+def test_get_text(nlp):
+    doc = nlp("The cat")
+    assert get_text(doc) == doc.text
--- weakvtg/tokenizer.py
+++ weakvtg/tokenizer.py
@@ -1,4 +1,11 @@
 from functools import partial
+from typing import Callable, Iterator, Any, List, Optional
+
+import spacy
+
+Doc = spacy.language.Doc
+Span = Any  # spacy does not export Span type
+ChunkingF = Optional[Callable[[Doc], Iterator[Span]]]
 
 
 def _spacy_nlp():
@@ -9,8 +16,6 @@ def _spacy_nlp():
     - [Customizing spaCy’s Tokenizer class](https://spacy.io/usage/linguistic-features#native-tokenizers)
     - [Modifying existing rule sets](https://spacy.io/usage/linguistic-features#native-tokenizer-additions)
     """
-    import spacy
-
     nlp = spacy.load('en_core_web_sm')
 
     prefixes = nlp.Defaults.prefixes + [r"""/""", r"""-"""]
@@ -30,3 +35,26 @@ def get_nlp():
 
 def get_torchtext_tokenizer_adapter(nlp):
     return partial(_spacy_tokenize, spacy=nlp)
+
+
+def get_noun_phrases(doc: Doc, f_chunking: ChunkingF = None) -> List[str]:
+    if f_chunking is None:
+        f_chunking = noun_chunk_iter
+
+    np = list(f_chunking(doc))
+    np = list(map(get_text, np))
+
+    return np
+
+
+def noun_chunk_iter(doc: Doc) -> Iterator[Span]:
+    return doc.noun_chunks
+
+
+def root_chunk_iter(doc: Doc) -> Iterator[Span]:
+    for chunk in noun_chunk_iter(doc):
+        yield chunk.root
+
+
+def get_text(doc: Doc) -> str:
+    return doc.text
