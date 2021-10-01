262902c1740c7668bde31a038b1f0176cb1c7629
lparolari <luca.parolari23@gmail.com>
2021-10-01 11:35:52 +0200

    feat: apply concept similarity with mean

---
+++ tests/model/test_apply_concept_similarity_mean.py
@@ -0,0 +1,23 @@
+import torch
+
+from weakvtg.model import apply_concept_similarity_mean
+
+
+def test_apply_concept_similarity_mean():
+    logits = torch.tensor([1, 2, 3])
+    concept_similarity = torch.tensor([4, 5, 6])
+
+    assert torch.equal(
+        apply_concept_similarity_mean(logits, concept_similarity),
+        (logits + concept_similarity) / 2
+    )
+
+
+def test_apply_concept_similarity_mean_given_weight():
+    logits = torch.tensor([1, 2, 3])
+    concept_similarity = torch.tensor([4, 5, 6])
+
+    assert torch.equal(
+        apply_concept_similarity_mean(logits, concept_similarity, lam=0.2),
+        (logits + 0.2 * concept_similarity) / 2
+    )
